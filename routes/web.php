37a5173ba647811d2addc41acd8ce8cbf3ba5a27
Anton Krivchik <anton4ik2251@yandex.ru>
2019-10-21 16:21:02 +0300

    start tag

--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin'/*, 'middleware'	=> 'auth'*/], function(){
     Route::get('/', 'DashboardController@index')->name('admin');
     Route::resource('/category', 'CategoriesController');
+    Route::resource('/tag', 'TagController');
 });
