cb853a759553fba4208e9196d03eb77f2707d772
Anton Krivchik <anton4ik2251@yandex.ru>
2019-10-21 12:49:04 +0300

    fix route

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 
-Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware'	=> 'auth'], function(){
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin'/*, 'middleware'	=> 'auth'*/], function(){
     Route::get('/', 'DashboardController@index')->name('admin');
     Route::resource('/category', 'CategoriesController');
 });
