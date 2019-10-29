bc1b015e3de8ca8b97e1cf822229b57418832e94
Anton Krivchik <anton4ik2251@yandex.ru>
2019-10-29 13:13:45 +0300

    debug 6

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -21,7 +21,7 @@ class Post extends Model
 
     //Атрибуты которые можно массово присваивать
     protected $fillable = [
-        'title','slug', 'content', 'status', 'views', 'date'
+        'title','slug', 'content', 'views', 'date'
     ];
 
     //Все поля разрешено менять
