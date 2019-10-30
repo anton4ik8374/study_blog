d59cca493aa99c78c3cda93f9be8a217c3ed80ff
Anton Krivchik <anton4ik2251@yandex.ru>
2019-10-30 12:31:43 +0300

    factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,7 +1,8 @@
 <?php
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-use App\User;
+use App\Models\User;
+use App\Models\Post;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
@@ -16,12 +17,26 @@ use Illuminate\Support\Str;
 |
 */
 
-$factory->define(User::class, function (Faker $faker) {
+/*$factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+    ];
+});*/
+
+$factory->define(Post::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence,
+        'content' => $faker->sentence,
+        'views' => $faker->numberBetween(1,5000),
+        'date' => date($format = 'd/m/y', $max = '29/10/19'),
+        'category_id' => array_rand([2,3,4,5,6,7]),
+        'tags' => [array_rand([7,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26]),array_rand([7,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26]),array_rand([7,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26])],
+        'user_id' => array_rand([1,7,9,11,13,14]),
+        'status' => 0,
+        'is_featured' => 0,
     ];
 });
