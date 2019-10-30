af8a5bed73276c7e550867125416ae397a89dc3f
Anton Krivchik <anton4ik2251@yandex.ru>
2019-10-30 13:06:40 +0300

    factory + seed fix + collection

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -11,8 +11,8 @@ $factory->define(Post::class, function (Faker $faker) {
         'content' => $faker->paragraphs(rand(5, 10), true),
         'views' => $faker->numberBetween(1,5000),
         'date' => mt_rand(1,30) . '/' . mt_rand(1,12) . '/' . mt_rand(10 , 19),
-        'category_id' => array_rand(array_flip([2,3,4,5,6,7])),
-        'user_id' => array_rand(array_flip([1,7,9,11,13,14])),
+        'category_id' => collect([2,3,4,5,6,7])->random(1),
+        'user_id' => collect([1,7,9,11,13,14])->random(1),
         'status' => 0,
         'is_featured' => 0,
     ];
