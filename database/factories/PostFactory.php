7ed8f3fa155b4cf02da28222d0080f90bf84ba35
Anton Krivchik <anton4ik2251@yandex.ru>
2019-10-30 12:43:48 +0300

    factory + seed

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Post;
+use Faker\Generator as Faker;
+
+$factory->define(Post::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence,
+        'content' => $faker->paragraphs(rand(5, 10), true),
+        'views' => $faker->numberBetween(1,5000),
+        'date' => date('d/m/y'),
+        'category_id' => array_rand([2,3,4,5,6,7]),
+        'user_id' => array_rand([1,7,9,11,13,14]),
+        'status' => 0,
+        'is_featured' => 0,
+    ];
+});
